888e02999a0da94da4202d8247099ca1a51312b5
NaamTuhin <tuhinmukk@gmail.com>
2024-12-19 12:56:49 +0530

    try to fix error

--- app/ui/font.ts
+++ app/ui/font.ts
@@ -1,4 +1,6 @@
 import { Inter,Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({weight:['400','700'], subsets: ['latin'] });
+export const lusitana = Lusitana(
+    {weight:['400','700'], subsets: ['latin'] }
+);
